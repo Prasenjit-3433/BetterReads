ed52ceda65a0f4f29257c8e377f996d7fae5bd74
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2024-04-09 13:39:33 +0530

    initialized data loader

---
+++ data-loader/src/main/java/com/betterreads/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.betterreads.betterreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterreadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
+	}
+
+}
