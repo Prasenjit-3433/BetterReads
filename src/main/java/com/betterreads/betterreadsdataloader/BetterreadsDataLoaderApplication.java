28a71901c0391d42d25f2b9f268eb44f7ec2e78c
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2024-04-16 13:33:04 +0530

    Using Repository pattern with Spring Data cassandra to manager author entity

--- src/main/java/com/betterreads/betterreadsdataloader/BetterreadsDataLoaderApplication.java
+++ src/main/java/com/betterreads/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -1,6 +1,10 @@
 package com.betterreads.betterreadsdataloader;
 
+import com.betterreads.betterreadsdataloader.author.Author;
+import com.betterreads.betterreadsdataloader.author.AuthorRepository;
 import com.betterreads.betterreadsdataloader.connection.DataStaxAstraProperties;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
@@ -13,8 +17,24 @@ import java.nio.file.Path;
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterreadsDataLoaderApplication {
 
+	@Autowired
+	AuthorRepository authorRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
+	}
+
+
+	@PostConstruct
+	public void start() {
+
+		// Testing:
+		Author author = new Author();
+		author.setId("id");
+		author.setName("name");
+		author.setPersonalName("personalName");
+
+		authorRepository.save(author);
 	}
 
 	@Bean
---
+++ src/main/java/com/betterreads/betterreadsdataloader/author/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.betterreads.betterreadsdataloader.author;
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends CassandraRepository<Author, String> {
+
+}
