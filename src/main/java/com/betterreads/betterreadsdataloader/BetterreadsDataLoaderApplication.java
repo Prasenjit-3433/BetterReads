1ca3d7d261e02c69244101eabbabeb886eec2763
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2024-04-18 10:45:50 +0530

    Saving all the authors in the astraDb cluster

--- src/main/java/com/betterreads/betterreadsdataloader/BetterreadsDataLoaderApplication.java
+++ src/main/java/com/betterreads/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -4,14 +4,21 @@ import com.betterreads.betterreadsdataloader.author.Author;
 import com.betterreads.betterreadsdataloader.author.AuthorRepository;
 import com.betterreads.betterreadsdataloader.connection.DataStaxAstraProperties;
 import jakarta.annotation.PostConstruct;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
@@ -20,21 +27,54 @@ public class BetterreadsDataLoaderApplication {
 	@Autowired
 	AuthorRepository authorRepository;
 
+	@Value("${datadump.location.author}")
+	private String authorDumpLocation;
+
+	@Value("${datadump.location.works}")
+	private String worksDumpLocation;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
 	}
 
+	private void initAuthors() {
+		Path path = Paths.get(authorDumpLocation);
+
+		try(Stream<String> lines = Files.lines(path)) {
+			lines.forEach(line -> {
+				// Read & parse the line
+				String jsonString = line.substring(line.indexOf("{"));
+
+				try {
+					JSONObject jsonObject = new JSONObject(jsonString);
+
+					// Construct an author object
+					Author author = new Author();
+					author.setName(jsonObject.optString("name"));
+					author.setPersonalName(jsonObject.optString("personal_name"));
+					author.setId(jsonObject.optString("key").replace("/authors/", ""));
+
+					// Persist using Repository
+					System.out.println("Saving author: " + author.getName() + "...");
+					authorRepository.save(author);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+
+			});
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void initWorks() {}
+
 
 	@PostConstruct
 	public void start() {
-
-		// Testing:
-		Author author = new Author();
-		author.setId("id");
-		author.setName("name");
-		author.setPersonalName("personalName");
-
-		authorRepository.save(author);
+		initAuthors();
+		initWorks();
 	}
 
 	@Bean
