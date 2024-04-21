7ca1a4574bdbfe2f1c2d62d0ef0e71d43b3baf20
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2024-04-21 13:55:35 +0530

    GitHub Oauth authentication for user session

--- src/main/java/com/betterreads/betterreadsdataloader/author/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.betterreads.betterreadsdataloader.author;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository extends CassandraRepository<Author, String> {
-
-}
--- src/main/java/com/betterreads/betterreadsdataloader/book/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.betterreads.betterreadsdataloader.book;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends CassandraRepository<Book, String> {
-
-}
